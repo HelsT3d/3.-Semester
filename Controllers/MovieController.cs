e37206e56891d44bfdf6450d7a11b14bc52310ae
Jeppe Helsted <jeppe@helsted.com>
2026-09-02 13:19:00 +0200

    Add project files.

---
+++ Controllers/MovieController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieMania.Models;
+using MovieMania.Persistence;
+
+namespace MovieMania.Controllers
+{
+    public class MovieController : Controller
+    {
+       
+        public IActionResult Index(int? MovieId)
+        {
+           List<Movie> movies = MovieRepository.GetAll();
+
+            return View(movies);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MovieMania
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{Controller=Home}/{Action=Index}/{Id?}");
+
+
+
+
+            app.Run();
+        }
+    }
+}
